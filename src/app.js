6b7b1544d7d10b12003e53a1eaededdac250e229
gohyinhao <yinhao.g@gmail.com>
2021-01-11 02:23:27 +0800

    complete parking options

--- src/app.js
+++ src/app.js
@@ -55,6 +55,18 @@ bot.onText(/\/car/, (msg) => {
           { text: '2A', callback_data: '2A' },
           { text: '2B', callback_data: '2B' },
         ],
+        [
+          { text: '3A', callback_data: '3A' },
+          { text: '3B', callback_data: '3B' },
+        ],
+        [
+          { text: '4A', callback_data: '4A' },
+          { text: '4B', callback_data: '4B' },
+        ],
+        [
+          { text: '5A', callback_data: '5A' },
+          { text: '5B', callback_data: '5B' },
+        ],
       ],
     },
   });
