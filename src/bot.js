869f32838eb5f11bab89e3a7548edd91ad875608
gohyinhao <yinhao.g@gmail.com>
2021-01-17 18:46:19 +0800

    fix: remove test token var

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
-// const token = process.env.TELEGRAM_TEST_ENV_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('polling_error', console.log);
