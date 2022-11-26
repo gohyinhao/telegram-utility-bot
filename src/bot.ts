03bc98a7be6a4a35772496fcdfce867d5a2a57ca
gohyinhao <yinhao.g@gmail.com>
2022-11-26 12:41:00 +0800

    fix: bot crash on launch

--- src/bot.ts
+++ src/bot.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const PORT = process.env.PORT || 3000;
-const URL = process.env.URL || 'https://telegram-utility-bot.herokuapp.com/';
+const URL = process.env.URL || 'https://telegram-utility-bot.herokuapp.com';
 
 const bot = new Telegraf(BOT_TOKEN || '');
 bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
@@ -17,7 +17,6 @@ bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT);
 bot.start((ctx) =>
   ctx.reply('Hello, I am the Goh Family Bot!\n' + 'Type /help for list of options!'),
 );
-bot.launch();
 
 // Enable graceful stop
 process.once('SIGINT', async () => {
